4080a5505e14f0fd42729d2bf624247746e5eff3
Martin Kunze <mth.kunze@gmail.com>
2017-11-28 14:15:01 +0100

    Initial Code

---
+++ README.md
@@ -0,0 +1,11 @@
+### Set my GIT global environment
+
+set_gitenv.sh
+
+Set global Develeper Git Environment
+
+# Variables
+
+User
+Email
+Core Editor
---
+++ set_gitenv.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+git config --global user.name "Martin Kunze"
+git config --global user.email mth.kunze@gmail.com
+git config --global core.editor vi
+
